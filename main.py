8c7a928dc7222d30d9ae18bd1882837a31d57705
Detrivinog <detrivinog@gmail.com>
2020-12-10 07:21:17 -0500

    update main

--- main.py
+++ main.py
@@ -12,6 +12,20 @@ from fastapi import HTTPException
 
 api = FastAPI()
 
+#### Conect with CORS
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+"http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+"http://localhost", "http://localhost:8080",
+]
+
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+####
+
+
 @api.post("/user/auth/")
 async def auth_user(user_in: UserIn):
     user_in_db = get_user(user_in.username)
